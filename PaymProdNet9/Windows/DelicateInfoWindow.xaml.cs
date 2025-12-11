b7494676a0e3a61889ecf9d4fbde5b8644becff9
Artyom K <a.karpets@sweedpos.com>
2025-12-11 19:22:30 +0500

    ач

---
+++ PaymProdNet9/Windows/DelicateInfoWindow.xaml.cs
@@ -0,0 +1,19 @@
+using System.Windows;
+
+namespace PaymProdNet9.Windows;
+
+public partial class DelicateInfoWindow : Window
+{
+    public DelicateInfoWindow(string delicateName, string composition)
+    {
+        InitializeComponent();
+        DelicateNameTextBlock.Text = delicateName;
+        CompositionTextBlock.Text = composition;
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
+
